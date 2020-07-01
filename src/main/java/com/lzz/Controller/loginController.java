f9d5bcec62c484ee33639b48ac157eb6e39dac54
lzz688 <lzz688@git.com>
2020-07-02 00:07:47 +0800

    注册表单

--- src/main/java/com/lzz/Controller/loginController.java
+++ src/main/java/com/lzz/Controller/loginController.java
@@ -9,8 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class loginController {
 
-//    @RequestMapping("/register")
-//    public String register(HttpServletRequest request){
-//        String exampleInputusername2 = request.getParameter("exampleInputusername2");
-//    }
+    @RequestMapping("/register")
+    public String register(HttpServletRequest request){
+        System.out.println("收到了注册请求");
+        String exampleInputusername2 = request.getParameter("exampleInputusername2");
+        String exampleInputPassword2 = request.getParameter("exampleInputPassword2");
+        String exampleInputuserage2 = request.getParameter("exampleInputuserage2");
+        String sex = request.getParameter("sex");
+        String exampleInputtelephone2 = request.getParameter("exampleInputtelephone2");
+        String exampleInputaddress2 = request.getParameter("exampleInputaddress2");
+        System.out.println(exampleInputusername2);
+        return null;
+    }
 }
